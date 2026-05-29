448ba160e607f9fdeb61354378f1cf48d11db98e
leesuhwan <acmquwjm@gmail.com>
2026-05-29 13:45:18 +0900

    first

---
+++ ch10/MethodTest.java
@@ -0,0 +1,17 @@
+package ch10;
+
+public class MethodTest {
+
+    void add() {
+        int n1 = 10;
+        int n2 = 100;
+
+        int sum = n1 + n2;
+
+        System.out.println(sum);
+    }
+
+    void add100(int n1){
+        int n2 = 100;
+    }
+}
